ccee32f658d88ea239f0aef8162cbcd61d8b5f8c
anliben <anliben.yayoi@gmail.com>
2022-05-09 16:38:45 -0300

    feature translate and fix card compoenent

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -12,23 +12,12 @@ export class HomePage implements OnInit {
   
   constructor(
     private router: Router,
-    private translate: TranslateService
   ) { }
 
   ngOnInit() {
-    this._initTranslate()
   }
 
   changeScreen(screen: any){
     this.router.navigate([screen])
   }
-  private _initTranslate(): void {
-    if (this.translate.getBrowserLang() !== undefined) {
-      let tr: any = this.translate.getBrowserLang()?.toString();
-      this.translate.use(tr);
-    } else {
-      this.translate.use('pt');
-    }
-  }
-
 }

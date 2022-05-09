458ae856c0017379655e0ea2b02bd21bf3b3f488
Tiago-msC <tiagomscunha@outlook.com>
2022-05-09 19:29:29 -0300

    [Upgrade]: Mudando local do componente carta-baralho e adicionando ao shared-module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,22 +4,22 @@ import { AvatarMesaComponent } from './components/avatar-mesa/avatar-mesa.compon
 import { HeaderComponent } from './components/header/header.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { IonicModule } from '@ionic/angular';
+import { CartaBaralhoComponent } from './components/carta-baralho/carta-baralho.component';
 
 @NgModule({
   declarations: [
     AvatarMesaComponent,
     HeaderComponent,
     PerfilComponent,
+    CartaBaralhoComponent,
   ],
-  imports: [
-    CommonModule,
-    IonicModule.forRoot()
-  ],
+  imports: [CommonModule, IonicModule.forRoot()],
   exports: [
     CommonModule,
     AvatarMesaComponent,
     HeaderComponent,
     PerfilComponent,
-  ]
+    CartaBaralhoComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}

6524d5d244f22f941f3835e145caef4e10eb5895
Tiago-msC <tiagomscunha@outlook.com>
2022-05-10 20:40:22 -0300

    [update]: criação do modal de configuração do jogo.

---
+++ src/app/shared/components/configuracao-jogo-modal/configuracao-jogo-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configuracao-jogo-modal',
+  templateUrl: './configuracao-jogo-modal.component.html',
+  styleUrls: ['./configuracao-jogo-modal.component.scss'],
+})
+export class ConfiguracaoJogoModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

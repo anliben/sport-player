267cade2176fbf7835c1a3ab5ffa41fbc8d056d6
Tiago-msC <tiagomscunha@outlook.com>
2022-05-12 15:09:23 -0300

    [update]: adicionando fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'background': "url('/assets/game/game/fundo-loja.png')",
+        background: "url('/assets/game/game/fundo-loja.png')",
         "background-titulo": "url('/assets/game/game/titulo.png')",
         "background-home": "url('/assets/game/game/fundo.png')",
-        'moeda': "url('/assets/game/game/fundo-loja.png')",
-        'diamante': "url('/assets/game/game/fundo-loja.png')",
-        'mesa_part_truco': "url('/assets/game/partidaTruco/mesa-fundo.png')",
-        "mesa": "url('/assets/game/partidaTruco/mesa-fundo.png')",
+        moeda: "url('/assets/game/game/fundo-loja.png')",
+        diamante: "url('/assets/game/game/fundo-loja.png')",
+        mesa_part_truco: "url('/assets/game/partidaTruco/mesa-fundo.png')",
+        mesa: "url('/assets/game/partidaTruco/mesa-fundo.png')",
+      },
+      fontFamily: {
+        vag: ["Vag-Round", "sans-serif"],
       },
     },
   },

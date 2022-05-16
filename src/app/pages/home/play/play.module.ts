9f99a20b88cedd90c02e273e052b0e9c62b36e3b
Tiago-msC <tiagomscunha@outlook.com>
2022-05-16 16:05:46 -0300

    [update]: Adicionando página play.

---
+++ src/app/pages/home/play/play.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PlayPageRoutingModule } from './play-routing.module';
+
+import { PlayPage } from './play.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PlayPageRoutingModule,
+    SharedModule,
+  ],
+  declarations: [PlayPage],
+})
+export class PlayPageModule {}

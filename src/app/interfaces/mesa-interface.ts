475363f835da177664abaa41c613b772ad7320e8
Tiago-msC <tiagomscunha@outlook.com>
2022-05-24 18:54:59 -0300

    [fix]: implementando a diferenciação das mesas

--- src/app/interfaces/mesa-interface.ts
+++ src/app/interfaces/mesa-interface.ts
@@ -1,19 +1,19 @@
 export interface MesaInterface {
-    id: 1;
-    name: string;
-    tableType: string;
-    awardTotal: string | number;
-    jackpot: string | number;
-    buy: string | number;
-    reBuy: false;
-    playersDistribution: string;
-    AreSpectatorsAllowed: boolean;
-    spectators: any;
-    start_date: string;
-    start_time: string;
-    update_at: string;
-    create_at: string;
-    status: string;
-    gameMode: string;
-    rules: string;
+  id: 1;
+  name: string;
+  tableType: string;
+  awardTotal: string | number;
+  jackpot: string | number;
+  buy: string | number;
+  reBuy: false;
+  playersDistribution: string;
+  areSpectatorsAllowed: boolean;
+  spectators: any;
+  startDate: string;
+  startTime: string;
+  updateAt: string;
+  createAt: string;
+  status: string;
+  gameMode: string;
+  rules: string;
 }
